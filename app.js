6366b7d9b74059965ae73f61dde3812df76f4d6f
LauraCZambrano <laurac3108@gmail.com>
2020-04-01 14:28:19 -0400

    first commit

---
+++ app.js
@@ -0,0 +1,23 @@
+//require
+const express = require('express');
+const http = require('http');
+const swaggerUi = require('swagger-ui-express');
+const swaggerDocument = require('./swagger.json');
+
+
+//initializations
+const app = express();
+
+
+//documentation
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
+//app.use('/api/v1', require('./Routes/index'));
+
+
+// Starting server http 
+const httpServer = http.createServer(app);
+
+//Server http
+httpServer.listen(8000, () => {
+    console.log('HTTP Server listen on port', 8000);
+});
